09d26ba7d5a48b2675523621865ed40ca0c48194
Amanda Hughes <amanda@trevi-app.com>
2025-07-16 13:02:26 +0100

    fix: update dependencies and add project files for EAS build

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import AppStack from './src/navigation/AppStack';
+import { StripeProvider } from '@stripe/stripe-react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          'Poppins-Light': require('./src/Assets/fonts/Poppins-Light.ttf'),
+          'Poppins-Regular': require('./src/Assets/fonts/Poppins-Regular.ttf'),
+          'Poppins-Medium': require('./src/Assets/fonts/Poppins-Medium.ttf'),
+          'Poppins-SemiBold': require('./src/Assets/fonts/Poppins-SemiBold.ttf'),
+          'Poppins-Bold': require('./src/Assets/fonts/Poppins-Bold.ttf'),
+        });
+      } catch (e) {
+        console.warn('❌ Font loading failed:', e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StripeProvider publishableKey="pk_test_51RXKLrIMEUGCmkevn3YDd0y1oRaPogoAAo5MpDFrMlfrM9YdO9ISBqrqaAl6kwoLQfQjScaaepDW8ZE0Tx7vyIKx00eiMFSmEZ">
+      <NavigationContainer onReady={onLayoutRootView}>
+        <AppStack />
+      </NavigationContainer>
+    </StripeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
